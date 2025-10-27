8ae8ead076a38a214cad0dce67392a6cb0115795
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2025-10-27 21:47:32 +0000

    feat: add initial file catalogue

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -16,6 +16,38 @@ export default function Upload() {
       />
 
       <button>Upload</button>
+
+      <h3 style={{ marginTop: 20 }}>Uploaded Files</h3>
+
+      <table
+        border={1}
+        cellPadding={6}
+        style={{
+          borderCollapse: "collapse",
+          width: "100%",
+          marginTop: 10,
+          fontSize: 14,
+        }}
+      >
+        <thread style={{ background: "#f0f0f038" }}>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Status</th>
+            <th>Uploaded At</th>
+            <th>Actions</th>
+          </tr>
+        </thread>
+        <tbody>
+            <tr>
+              <td colSpan="6" style={{ textAlign: "center", color: "#666666ff" }}>
+                No uploads yet
+              </td>
+            </tr>
+        </tbody>
+      </table>
+      
     </div>
   );
 }
