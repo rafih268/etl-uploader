fa2e0134b8d62b1ed4ceac5d960ba3d3d861a74e
Imtiaj Rafi Hashem <zjac260@live.rhul.ac.uk>
2025-10-31 14:23:29 +0000

    feat(etl): add request handler for running the etl

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,13 @@ import fs from 'fs';
 import dotenv from 'dotenv';
 import { pool } from './db.js';
 import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
 
 dotenv.config();
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
+const PYTHON_EXEC = process.env.PYTHON_EXEC || "python";
 
 const PORT = process.env.PORT || 4000;
 
@@ -71,6 +73,45 @@ app.get('/api/catalogue', async (req, res) => {
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
+  }
+});
+
+app.post('/api/etl/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const response = await pool.query('SELECT * FROM uploads WHERE id=$1', [id]);
+    if (response.rowCount === 0) return res.status(404).json({ error: 'File not found' });
+
+    const upload = response.rows[0];
+    const filePath = upload.path;
+
+    console.log(`Initiating ETL for ${filePath}`);
+
+    res.json({
+      message: `ETL triggered for file id ${id}`,
+      file: filePath
+    });
+
+    const python = spawn(PYTHON_EXEC, ["../etl/etl_runner.py", filePath, id]);
+
+    python.stdout.on("data", (data) => {
+      console.log(`[ETL STDOUT]: ${data}`);
+    });
+
+    python.stderr.on("data", (data) => {
+      console.log(`[ETL STDERR]: ${data}`);
+    });
+
+    python.on("close", async (code) => {
+      console.log(`ETL process for ${id} finished with code ${code}`);
+
+      const newStatus = code === 0 ? "processed" : "failed";
+      await pool.query("UPDATE uploads SET status=$1 WHERE id=$2", [newStatus, id]);
+      console.log(`File ${id} status updated to '${newStatus}'`);
+    }); 
+  } catch (err) {
+    console.error('ETL API error:', err);
+    return res.status(500).json({ error: err.message });
   }
 });
 
